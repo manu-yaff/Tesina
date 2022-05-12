e72ef68607b5f140c91d074261562c5db086110d
manu-yaff <manuelyg2097@gmail.com>
2022-05-11 20:28:58 -0500

    Add new structure and generate plot script

--- README.md
+++ README.md
@@ -1,36 +1,10 @@
-# Tesina
-## Guía de uso Linux
+Steps that I did
 
+- Create a virtual environment
+	- python -m venv .venv
 
-## Librerias a instalar
-      
-### Python (pip)
-* virtualenv           20.4.6           
-* Fiona                  1.8.19                       
-* geopandas              0.9.0                           
-* matplotlib             3.4.1                                             
-* pandas                 1.2.4                                             
-* pyproj                 3.0.1               
-* shapely                1.7.1         
+- Activar virtual environment
+- source .venv/bin/actiavte
 
-### PC (apt install)
-* pip           20.0.2 
-* ffmpeg 		 4.2.4 
-* python3-venv      3.8.5
-* python3-tk           3.8.5
-
-## Ejecutar herramientas
-
-1. Ejecutar archivo install.txt
-
-2. Crear y activar virtual environment
-
-3. Clonar repositorio
-
-4. Ejecutar comando: python3 visPops.py
-   
-5. Mostrará en pantalla el plot
-
-6. Ejecutar comando: python3 visSimul.py
-
-7. Esperar a que genere el video de la simulación con el nombre output.mp4
+- Instalar el requirements.txt
+ - pip install -r requirements.txt
---
+++ populations_visualization.py
@@ -0,0 +1,141 @@
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import pandas as pd
+import bz2
+import _pickle as cPickle
+from shapely.geometry import Point, Polygon
+from pyproj import CRS
+
+
+def read_map_shape_file(file):
+    """
+    This function reads a shapefile (.shp)
+
+    :param file: path of the shape file to read
+    :return: geoDataFrame object
+    """
+    return gpd.read_file(file)
+
+def read_populations_file(file):
+    """
+    This function reads the populations file (.csv)
+
+    :param file: path of the populations file
+    :return: DataFrame object
+    """
+    return pd.read_csv(file)
+
+def read_clusters_file(file):
+    """
+    This function reads the clusters file (.bz)
+
+    :param file: path of the clusters file
+    :return: array of arrays describing which populations belong to which cluster
+    """
+    with bz2.open(file, "rb") as f:
+        content = f.read()
+    return cPickle.loads(content)
+
+def generate_geo_populations_data_frame(populations, crs):
+    """
+    This function makes a geoDataFrame from a given populations dataFrame
+
+    :param populations: the populations dataFrame
+    :param crs: specifies the the Coordinate Reference System
+    :return: geoDataFrame,  
+    """
+    points = [Point(xy) for xy in zip(populations["lon"], populations["lat"])]
+    return gpd.GeoDataFrame(populations, crs=crs, geometry=points)
+
+def generate_centroids(clusters, populations):
+    """
+    This function genrates the centroids and their coordinates
+
+    :param clusters: clusters array
+    :param populations: the populations dataFrame
+    :return: centroids points and their coordinates
+    """
+    centroids = []
+    coordinates = []
+    centroid_id = 0
+
+    for cluster in clusters:
+        accumLongitude = 0
+        accumLatitude = 0
+        population = 0
+
+        for population in cluster:
+            point = populations.iloc[population]
+            accumLongitude += point.lon
+            accumLatitude += point.lat
+            population += point["pop"]
+
+        centroidLongitude = accumLongitude / len(cluster)
+        centroidLatitude = accumLatitude / len(cluster)
+        coordinate = [centroid_id, centroidLongitude, centroidLatitude, population]
+        point = Point(centroidLongitude, centroidLatitude)
+        centroids.append(point)
+        coordinates.append(coordinate)
+        centroid_id += 1
+
+    return [centroids, coordinates]
+
+def generate_geo_centroids_data_frame(centroids, coordinates):
+    """
+    This function creates a geoDataFrame with the clusters centroids
+
+    :param centroids: array of Point objects
+    :param coordinates: array of centroids coordinates
+    :return: centroids geoDataFrame
+    """
+    columns = ['id', 'lon', 'lat', 'population']
+    df = pd.DataFrame(data=coordinates, columns=columns)
+    return gpd.GeoDataFrame(df, crs=crs, geometry=centroids)
+
+def generate_plot(map_shape, geo_populations, populations, centroids, geo_centroids):
+    """
+    This function builds and shows the plot
+
+    :param map_shape: map shape object
+    :param geo_populations: geo populations data frame
+    :param populations: populations data frame
+    :param centroids: centroids points and their coordinates
+    """
+    fig, ax = plt.subplots(figsize=(6,6))
+    map_shape.plot(ax=ax, alpha=0.4, figsize=(20, 15), edgecolor="lightgray", facecolor="lightgray")
+    geo_populations.plot(ax=ax, markersize=20, color=(0.4, 0.2, 0.5, 0.2), marker="o", label="Population")
+    pointLabel = 0
+    for i in range(len(populations)):
+        point = populations.iloc[i]
+        plt.text(point["lon"], point["lat"], str(pointLabel), color=(0.4, 0.2, 0.5, 0.2), fontsize=6)
+        pointLabel += 1
+
+    geo_centroids.plot(ax=ax, markersize=60, color="steelblue", marker="X", label="Cluster")
+
+    for coordinate in centroids[1]:
+        plt.text(coordinate[1], coordinate[2], coordinate[0], color="black", fontsize=8)
+
+    plt.legend(prop={'size': 10}, loc="upper left")
+    plt.show()
+
+# temp variables
+shape_file = './shapefile/stp_gc_adg.shp'
+populations_file = './populations.csv'
+clusters_file = './clusters.bz'
+crs = CRS('EPSG:4326')
+
+def main():
+    map_shape = read_map_shape_file(shape_file)
+    populations = read_populations_file(populations_file)
+    clusters =read_clusters_file(clusters_file)
+    geo_populations = generate_geo_populations_data_frame(populations, crs)
+    centroids = generate_centroids(clusters, populations)
+    geo_centroids = generate_geo_centroids_data_frame(centroids[0], centroids[1])
+    generate_plot(map_shape, geo_populations, populations, centroids, geo_centroids)
+
+main()
+
+
+
+
+
